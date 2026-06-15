bf18d891711a921dec48ab4270b65e0974d9ac7b
Arkady Vaisberg <arkadyvaisberg@MacBookAir.lan>
2026-06-15 13:09:22 +0300

    Add HSTS header to enforce HTTPS in browsers

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
