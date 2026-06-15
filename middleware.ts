3b92b97c736721555d783c8abc3120a8a92f2a0c
Arkady Vaisberg <arkadyvaisberg@MacBookAir.lan>
2026-06-15 12:45:47 +0300

    Force HTTPS redirect via middleware

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.headers.get("x-forwarded-proto") === "http") {
+    return NextResponse.redirect(
+      `https://${request.headers.get("host")}${request.nextUrl.pathname}${request.nextUrl.search}`,
+      301
+    );
+  }
+}
+
+export const config = {
+  matcher: "/:path*",
+};
